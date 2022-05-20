a5e14f13871fd09de7886f80a08775bf790ec520
Andrew Browne <3808083+frankenwino@users.noreply.github.com>
2022-05-20 18:53:44 +0200

    All functions and menu complete except for the change text colour function.

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
-from datetime import datetime
-import random
 import os
+import random
+import sys
+from datetime import datetime
 
 def input_alpha_string(question_text):
     """
@@ -76,20 +77,28 @@ def hello_world():
     """
     1. Funktion som skriver ut ”Hello World” i konsolen.
     """
+    clear_screen()
+    print(f"{hello_world.__doc__.strip()}\n")
 
     print("Hello World!")
+
+    return_to_menu()
 
 
 def input_from_user():
     """
-    2. Funktion som tar in input från användaren (Förnamn, Efternamn, Ålder)
-    och sedan skriver ut dessa i konsolen.
+    2. Funktion som tar in input från användaren (Förnamn, Efternamn, Ålder) och
+    sedan skriver ut dessa i konsolen.
     """
+    clear_screen()
+    print(f"{input_from_user.__doc__.strip()}\n")
 
     forename = input_alpha_string("Enter your forename: ")
     surname = input_alpha_string("Enter your surname: ")
     age = input_integer("Enter your age: ")
     print(f"Hello {forename} {surname}. You are {age} years old.")
+
+    return_to_menu()
 
 
 def change_text_colour():
@@ -98,7 +107,12 @@ def change_text_colour():
     3. Funktion som ändrar färgen på texten i konsolen (och ändrar tillbaka om
     man använder funktionen igen).
     """
-    pass
+
+    clear_screen()
+    print(f"{change_text_colour.__doc__.strip()}\n")
+
+    print("TO DO!")
+    return_to_menu()
 
 
 def todays_date():
@@ -106,8 +120,13 @@ def todays_date():
     4. Funktion för att skriva ut dagens datum.
     """
 
-    todays_date = datetime.now().strftime("%Y-%m-%d")
-    print(f"Date: {todays_date}")
+    clear_screen()
+    print(f"{todays_date.__doc__.strip()}\n")
+
+    the_date = datetime.now().strftime("%Y-%m-%d")
+    print(f"Date: {the_date}")
+
+    return_to_menu()
 
 
 def print_largest():
@@ -116,16 +135,27 @@ def print_largest():
     störst.
     """
 
+    clear_screen()
+    print(f"{print_largest.__doc__.strip()}\n")
+
     integer1 = input_integer("Enter first integer: ")
     integer2 = input_integer("Enter second integer: ")
 
-    if integer1 > integer2:
+    if integer1 == integer2:
+        largest = None
+    elif integer1 > integer2:
         largest = integer1
     else:
         largest = integer2
 
     print(f"Two integers were entered: {integer1} and {integer2}")
-    print(f"The largest integer is {largest}")
+
+    if largest is None:
+        print(f"The integers are the same.")
+    else:
+        print(f"The largest integer is {largest}")
+
+    return_to_menu()
 
 
 def number_guesser():
@@ -136,10 +166,13 @@ def number_guesser():
     meddelande visas som informerar ifall talet var för stort eller för litet.
     """
 
+    clear_screen()
+    print(f"{number_guesser.__doc__.strip()}\n")
+
     random_number = random.randint(1, 100)
     guesses = 0
 
-    print(f"Hint: the random number is {random_number}")
+    print(f"Hint: the random number is {random_number}\n")
 
     while True:
         user_guess = input_integer("Guess the number between 1 - 100: ")
@@ -159,7 +192,9 @@ def number_guesser():
     else:
         guess_text = "guess"
 
-    print(f"Congratulations! It took {guesses} {guess_text} to get the right number!")
+    print(f"\nCongratulations! It took {guesses} {guess_text} to get the right number!")
+
+    return_to_menu()
 
 
 def write_text_to_file(text_file):
@@ -171,12 +206,16 @@ def write_text_to_file(text_file):
         text_file (str): Path of the file being written to.
     """
 
+    clear_screen()
+    print(f"{write_text_to_file.__doc__.strip()}\n")
+
     print("Write a line of text. We're going to save that text to a file.")
     text = input("\n\n> ")
     with open(text_file, "w") as f:
         f.write(text)
-
     print(f"\nFile saved to {text_file}")
+
+    return_to_menu()
 
 
 def read_text_from_file(text_file):
@@ -187,6 +226,9 @@ def read_text_from_file(text_file):
     Parameters:
         text_file (str): Path of the file being read from.
     """
+
+    clear_screen()
+    print(f"{read_text_from_file.__doc__.strip()}\n")
 
     if not os.path.isfile(text_file):
         print("Error! No text file found.")
@@ -201,6 +243,8 @@ def read_text_from_file(text_file):
 
     print(f"> {data} <\n")
 
+    return_to_menu()
+
 
 def decimal_calculations():
     """
@@ -208,15 +252,20 @@ def decimal_calculations():
     ur, upphöjt till 2 och upphöjt till 10.
     """
 
+    clear_screen()
+    print(f"{decimal_calculations.__doc__.strip()}\n")
+
     decimal = input_decimal("Enter a decimal number: ")
     decimal_square_root = decimal ** (1/2)
     power_of_two = decimal ** 2
     power_of_ten = decimal ** 10
 
-    print(f"Decimal: {decimal}")
+    # print(f"Decimal: {decimal}")
     print(f"Square root of {decimal}: {decimal_square_root}")
     print(f"{decimal} raised to the power of 2: {power_of_two}")
     print(f"{decimal} raised to the power of 10: {power_of_ten}")
+
+    return_to_menu()
 
 
 def decimal_calculations_using_python_modules():
@@ -224,6 +273,9 @@ def decimal_calculations_using_python_modules():
     9. Funktion där användaren skickar in ett decimaltal och får tillbakaroten
     ur, upphöjt till 2 och upphöjt till 10.
     """
+
+    clear_screen()
+    print(f"{decimal_calculations_using_python_modules.__doc__.strip()}\n")
 
     import math
 
@@ -237,6 +289,8 @@ def decimal_calculations_using_python_modules():
     print(f"{decimal} raised to the power of 2: {power_of_two}")
     print(f"{decimal} raised to the power of 10: {power_of_ten}")
 
+    return_to_menu()
+
 
 def multiplication_table():
     """
@@ -244,9 +298,15 @@ def multiplication_table():
     10. En ”tabb”ska läggas in efter varje nummer. Försöka att ställa upp det så
     det blir relativt läsbart.
     """
+
+    clear_screen()
+    print(f"{multiplication_table.__doc__.strip()}\n")
+
     for i in range(1, 2):
         for j in range(1, 11):
             print(f"{i} * {j} = {1*j}")
+
+    return_to_menu()
 
 
 def bubble_sort_integers(integer_list):
@@ -276,12 +336,17 @@ def integer_list_sort():
     Array.Sort() får EJ användas.
     """
 
-    random_integer_list = [random.randint(1, 100) for i in range(11)]
+    clear_screen()
+    print(f"{integer_list_sort.__doc__.strip()}\n")
+
+    random_integer_list = [random.randint(-100, 100) for i in range(11)]
     random_integer_list_copy = random_integer_list.copy()
     sorted_integer_list = bubble_sort_integers(random_integer_list_copy)
 
-    print(f"Unsorted list:\t{random_integer_list}")
-    print(f"Sorted list:\t{sorted_integer_list}")
+    print(f"Random unsorted integer list:\t{random_integer_list}")
+    print(f"Sorted integer list:\t\t{sorted_integer_list}")
+
+    return_to_menu()
 
 
 def palindrome():
@@ -289,6 +354,9 @@ def palindrome():
     12. Funktion som tar en input från användaren och kontrollerar ifall det är
     en palindrom (alltså samma ord från båda håll, såsom Anna eller radar).
     """
+
+    clear_screen()
+    print(f"{palindrome.__doc__.strip()}\n")
 
     user_input_raw = input_alpha_string("Enter a word: ")
     user_input_sanitised = user_input_raw.lower()
@@ -300,6 +368,8 @@ def palindrome():
 
     print(f"{user_input_raw} is a palindrome: {is_palindrome}")
 
+    return_to_menu()
+
 
 def intervening_integers():
     """
@@ -307,19 +377,31 @@ def intervening_integers():
     siffror som är mellan de två inputsen.
     """
 
+    clear_screen()
+    print(f"{intervening_integers.__doc__.strip()}\n")
+
+
     integer1 = input_integer("Enter first integer: ")
     integer2 = input_integer("Enter second integer: ")
-
     intervening_integer_list = []
 
-    integer1 += 1
-    while integer1 < integer2:
-        intervening_integer_list.append(integer1)
-        integer1 += 1
-
     print(f"Start integer: {integer1}")
-    print(f"Intervening integers: {intervening_integer_list}")
+
+    if integer1 < integer2:
+        integer1 += 1
+        while integer1 < integer2:
+            intervening_integer_list.append(str(integer1))
+            integer1 += 1
+    else:
+        integer1 -= 1
+        while integer1 > integer2:
+            intervening_integer_list.append(str(integer1))
+            integer1 -= 1
+
+    print(f"Intervening integers: {', '.join(intervening_integer_list)}")
     print(f"End integer: {integer2}")
+
+    return_to_menu()
 
 
 def convert_user_integer_input_string_to_list(user_input):
@@ -346,6 +428,9 @@ def sort_user_integer_input_string_into_odd_and_even_integers():
     siffror) som sedan sorteras och skrivs ut efter udda och jämna värden.
     """
 
+    clear_screen()
+    print(f"{sort_user_integer_input_string_into_odd_and_even_integers.__doc__.strip()}\n")
+
     while True:
         try:
             user_input = input("Enter a list of comma separated integers: ")
@@ -367,10 +452,12 @@ def sort_user_integer_input_string_into_odd_and_even_integers():
         else:
             odd_list.append(i)
 
-    print(f"Orig:\t{user_integer_list}")
-    print(f"Sorted:\t{sorted_integer_list}")
-    print(f"Even:\t{even_list}")
-    print(f"Odd:\t{odd_list}")
+    print(f"User's integer list:\t{user_integer_list}")
+    print(f"Sorted integers:\t{sorted_integer_list}")
+    print(f"Even integers:\t\t{even_list}")
+    print(f"Odd integers:\t\t{odd_list}")
+
+    return_to_menu()
 
 
 def add_user_integer_input_string_together():
@@ -378,6 +465,9 @@ def add_user_integer_input_string_together():
     15. Funktion där användaren skriver in ett antal värden(komma-separerade
     siffor) som sedan adderas och skrivs ut.
     """
+
+    clear_screen()
+    print(f"{add_user_integer_input_string_together.__doc__.strip()}\n")
 
     while True:
         try:
@@ -388,15 +478,15 @@ def add_user_integer_input_string_together():
             print("Invalid data! Please enter comma separated integers e.g. 33, 5, 42, 99, -1746")
             continue
 
-    print(f"Orig:\t{user_integer_list}")
-    print(f"Add:\t {sum(user_integer_list)}")
+    print(f"User's integer list:\t{user_integer_list}")
+    print(f"Sum of integers:\t {sum(user_integer_list)}")
+
+    return_to_menu()
 
 
 class Character(object):
     """
     A class to represent a character.
-
-    ...
 
     Attributes
     ----------
@@ -407,12 +497,16 @@ class Character(object):
     strength : int
         the character's strength stat
     luck : int
-        the character's health luck
+        the character's luck
 
     Methods
     -------
-    info(additional=""):
-        Prints the person's name and age.
+    update_health_score(value):
+        Updates the character's health score
+    update_strength_score(value):
+        Updates the character's strength score.
+    update_luck_score(value):
+        Updates the character's luck score.
     """
 
     def __init__(self, name, health, strength, luck):
@@ -432,7 +526,7 @@ class Character(object):
 
 
 class Hero(Character):
-    """docstring for ."""
+    """A Character subclass that represents a Hero character"""
 
     def __init__(self, name, health, strength, luck):
         Character.__init__(self, name, health, strength, luck)
@@ -440,7 +534,7 @@ class Hero(Character):
 
 
 class Villain(Character):
-    """docstring for ."""
+    """A Character subclass that represents a Villain character"""
 
     def __init__(self, name, health, strength, luck):
         Character.__init__(self, name, health, strength, luck)
@@ -454,8 +548,11 @@ def character_generator():
     Hälsa, Styrka och Tur, som sparas i en instans av en klass.
     """
 
-    hero_name = "Andrew"  # input("Please enter the name of the hero: ")
-    villain_name = "Werdna"  # input("Please enter the name of the villain")
+    clear_screen()
+    print(f"{character_generator.__doc__.strip()}\n")
+
+    hero_name = input("Please enter the name of the hero:\t")
+    villain_name = input("Please enter the name of the villain:\t")
 
     player1 = Hero(
         name=hero_name,
@@ -463,6 +560,7 @@ def character_generator():
         strength=random.randint(1, 10),
         luck=random.randint(1, 10)
         )
+
     player2 = Villain(
         name=villain_name,
         health=random.randint(1, 10),
@@ -473,18 +571,107 @@ def character_generator():
     player_list = [player1, player2]
 
     for player in player_list:
-        print(f"Name: {player.name}")
+        print(f"\nName: {player.name}")
         print(f"Type: {player.character_type}")
-        print(f"Starting Health: {player.health}")
-        print(f"Starting Strength: {player.strength}")
-        print(f"Starting Luck: {player.luck}")
-        print()
+        print(f"Health: {player.health}")
+        print(f"Strength: {player.strength}")
+        print(f"Luck: {player.luck}")
 
+    return_to_menu()
+
+
+def return_to_menu():
+    input("\nPress Enter to return to the menu. ")
+
+
+def clear_screen():
+    if os.name == "nt":
+        os.system("cls")
+    else:
+        os.system("clear")
+
+
+def print_menu():
+    clear_screen()
+    for key in menu_options.keys():
+        print (f'{key}.\t', menu_options[key] )
+
+def exit():
+    print("Exiting. Bye!")
+    sys.exit(0)
+
+
+working_dir = os.path.dirname(os.path.abspath(__file__))
+text_file = os.path.join(working_dir, "text_file.txt")
+valid_menu_options = [x for x in range(0,17)]
+menu_options = {
+    1: 'hello_world()',
+    2: 'input_from_user()',
+    3: 'change_text_colour() TODO',
+    4: 'todays_date()',
+    5: 'print_largest()',
+    6: 'number_guesser()',
+    7: 'write_text_to_file(text_file)',
+    8: 'read_text_from_file(text_file)',
+    9: 'decimal_calculations()',
+    10: 'multiplication_table()',
+    11: 'integer_list_sort()',
+    12: 'palindrome()',
+    13: 'intervening_integers()',
+    14: 'sort_user_integer_input_string_into_odd_and_even_integers()',
+    15: 'add_user_integer_input_string_together()',
+    16: 'character_generator()',
+    0: 'Exit',
+}
 
 if __name__ == "__main__":
-
-    working_dir = os.path.dirname(os.path.abspath(__file__))
-    text_file = os.path.join(working_dir, "text_file.txt")
+    while True:
+        try:
+            print_menu()
+            option = int(input('\nEnter your choice [0-16]: '))
+            if option in valid_menu_options:
+                if option == 1:
+                    hello_world()
+                elif option == 2:
+                    input_from_user()
+                elif option == 3:
+                    change_text_colour()
+                elif option == 4:
+                    todays_date()
+                elif option == 5:
+                    print_largest()
+                elif option == 6:
+                    number_guesser()
+                elif option == 7:
+                    write_text_to_file(text_file)
+                elif option == 8:
+                    read_text_from_file(text_file)
+                elif option == 9:
+                    decimal_calculations()
+                elif option == 10:
+                    multiplication_table()
+                elif option == 11:
+                    integer_list_sort()
+                elif option == 12:
+                    palindrome()
+                elif option == 13:
+                    intervening_integers()
+                elif option == 14:
+                    sort_user_integer_input_string_into_odd_and_even_integers()
+                elif option == 15:
+                    add_user_integer_input_string_together()
+                elif option == 16:
+                    character_generator()
+                elif option == 0:
+                    exit()
+                else:
+                    continue
+            else:
+                input("Error! Please enter a value between 0-16. Press Enter to continue. ")
+                continue
+        except ValueError:
+            input("Error! Please enter a value between 0-16. Press Enter to continue. ")
+            continue
 
     # hello_world()  # 1
     # input_from_user()  # 2
